d90d2fb9845b5263e1a78e4be550ca5ddc89d4ad
mloibner <m.loibner@hotmail.com>
2020-08-17 00:02:57 +1000

    Updating changes, going to bed and finishing tomorrow

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require ("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const burgerObject = {
+            burgers: data
+        };
+        console.log(burgerObject);
+        res.render("index", burgerObject);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
+        res.json({ id: result.insertID});
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update(
+      {
+        burger_name: req.body.burger_name
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+  
