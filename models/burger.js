e8f97d321c129470c5bd59b28a0d6dea18949546
mloibner <m.loibner@hotmail.com>
2020-08-17 00:06:58 +1000

    models completed

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,22 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    
-}
+    selectAll: (cb) => {
+      orm.all("burger", (res) => {
+        cb(res);
+      });
+    },
+    insertOne: (cols, vals, cb) => {
+      orm.create("burgers", cols, vals, (res) => {
+        cb(res);
+      });
+    },
+    updateOne: (objColVals, condition, cb) => {
+      orm.update("burgers", objColVals, condition, (res) => {
+        cb(res);
+      });
+    }
+  };
+  
+  module.exports = burger;
+  
