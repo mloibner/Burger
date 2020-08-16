9a69a4ace1513cfe444c4d2931c93df01bfa124a
mloibner <m.loibner@hotmail.com>
2020-08-16 23:23:30 +1000

    mysql completed and db created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Tripple Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Schnitzel');
