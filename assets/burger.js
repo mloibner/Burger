146de61ea5a05d50163f658ed7cc493f635394d5
mloibner <m.loibner@hotmail.com>
2020-08-23 19:08:18 +1000

    adding all changes, code still not working correctly

--- assets/burger.js
+++ assets/burger.js
@@ -1,60 +1,47 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(() => {
   $(".change-burger").on("click", function(event) {
-    const id = $(this).data("id");
-    const newburger = $(this).data("newburger");
+      const id = $(this).data("id");
+      const newburger = $(this).data("newburger");
 
-    const newburgerState = {
-      devour: newburger
-    };
+      const newBurgerState = {
+          devour: newburger
+      };
 
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newburgerState
-    }).then(
-      () => {
-        console.log("changed burger to", newburger);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newBurgerState
+      }).then(
+          () => {
+              console.log("changed burger to", newburger);
+              // Reload the page to get the updated list
+              location.reload();
+          }
+      );
   });
 
   $(".create-form").on("submit", (event) => {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
 
-    const newBurger = {
-      name: $("#bu").val().trim(),
-      devour: $("[name=devour]:checked").val().trim()
-    };
+      const newBurger = {
+          burger_name: $("#bu").val().trim(),
+          devour: $("[name=devour]:checked").val().trim()
+      };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      () => {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+      }).then(
+          () => {
+              console.log("created new burger");
+              // Reload the page to get the updated list
+              location.reload();
+          }
+      );
   });
 
-  $(".delete-burger").on("click", function(event) {
-    const id = $(this).data("id");
 
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      () => {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 });
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name) VALUES ('Tripple Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Chicken Schnitzel');
+INSERT INTO  burgers (burger_name, devour) VALUES ('Schnitzel Burger', false);
+INSERT INTO  burgers (burger_name, devour) VALUES ('Tripple Cheese Burger', false);
+INSERT INTO  burgers (burger_name, devour) VALUES ('Bacon Burger', false);
+
+INSERT INTO burgers (burger_name, devour) VALUES ('Truffle Mayo Beef Burger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Zinger Burger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Crispy Chicken Clubhouse Burger', true);
