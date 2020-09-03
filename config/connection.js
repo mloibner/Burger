c43c90af3b3ccbfc16f2ec2d99bb5e8c752fb32a
Monique Loibner <m.loibner@hotmail.com>
2020-09-03 19:57:14 +1000

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
+if(process.envJAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
